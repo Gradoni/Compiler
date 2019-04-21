8a08bfbce5b295a797303cae77d35330cb4a7a27
Igor <igortrotskii@gmail.com>
2019-04-21 10:44:23 +0300

    Fixed multisign operators

---
+++ UnitTests/UnitTests.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Compiler/lexer.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(UnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+	};
+}
